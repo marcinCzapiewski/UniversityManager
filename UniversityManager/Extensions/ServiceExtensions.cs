05bbeed1576712a03c6bce21d10280d98730f772
Marcin Czapiewski <marcin.karol.czapiewski@gmail.com>
2019-04-29 03:16:25 +0200

    Configure connection with db

---
+++ UniversityManager/Extensions/ServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniversityManager.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureMySqlContext(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<RepositoryContext>(options => options.UseSqlServer(config.GetConnectionString("SqlServer")));
+        }
+    }
+}
