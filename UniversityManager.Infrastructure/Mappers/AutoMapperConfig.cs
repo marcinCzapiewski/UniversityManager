22ea89b8c85b8bface3a422c79cfcdd843268bcb
Marcin Czapiewski <marcin.karol.czapiewski@gmail.com>
2019-04-29 23:12:02 +0200

    Add automapper

---
+++ UniversityManager.Infrastructure/Mappers/AutoMapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace UniversityManager.Infrastructure.Mappers
+{
+    public static class AutoMapperConfig
+    {
+        public static IMapper Initialize()
+            => new MapperConfiguration(config =>
+            {
+
+            })
+            .CreateMapper();
+    }
+}
