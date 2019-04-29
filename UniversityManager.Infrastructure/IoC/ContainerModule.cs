44956bea0b5dba732f0e573e8708350c9509c28a
Marcin Czapiewski <marcin.karol.czapiewski@gmail.com>
2019-04-29 23:21:41 +0200

    Add autofac

---
+++ UniversityManager.Infrastructure/IoC/ContainerModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using AutoMapper.Configuration;
+using UniversityManager.Infrastructure.Mappers;
+
+namespace UniversityManager.Infrastructure.IoC
+{
+    public class ContainerModule : Module
+    {
+        private readonly IConfiguration _configuration;
+
+        public ContainerModule(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterInstance(AutoMapperConfig.Initialize()).SingleInstance();
+        }
+    }
+}
