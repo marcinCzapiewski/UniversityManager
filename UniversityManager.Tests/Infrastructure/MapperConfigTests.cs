23ec013eeeb6edbe15692e517fb5d079d8cd9343
Marcin Czapiewski <marcin.karol.czapiewski@gmail.com>
2019-04-30 21:12:08 +0200

    Created automapper config test

---
+++ UniversityManager.Tests/Infrastructure/MapperConfigTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UniversityManager.Infrastructure.Mappers;
+using Xunit;
+
+namespace UniversityManager.Tests.Infrastructure
+{
+    public class MapperConfigTests
+    {
+        [Fact]
+        public void when_mapper_config_is_setup_configuration_should_be_valid()
+        {
+            var config = AutoMapperConfig.Initialize();
+
+            config.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+    }
+}
