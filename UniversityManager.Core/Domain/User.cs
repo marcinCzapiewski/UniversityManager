c2a126f2146d1f5cefc937f5f15a048844d4ea32
Marcin Czapiewski <marcin.karol.czapiewski@gmail.com>
2019-05-09 13:26:41 +0200

    Add User Migration

--- UniversityManager.Core/Domain/User.cs
+++ UniversityManager.Core/Domain/User.cs
@@ -114,4 +114,3 @@ namespace UniversityManager.Core.Domain
         }
     }
 }
-}
---
+++ UniversityManager.Infrastructure/Migrations/20190509111837_AddUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UniversityManager.Infrastructure.Migrations
+{
+    public partial class AddUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Salt = table.Column<string>(nullable: true),
+                    Username = table.Column<string>(nullable: true),
+                    FullName = table.Column<string>(nullable: true),
+                    Role = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
