60c4454dbfba0512d54730c073d977412e5821a3
Marcin Czapiewski <marcin.karol.czapiewski@gmail.com>
2019-05-12 17:21:04 +0200

    Add Users Controller

---
+++ UniversityManager.Api/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using UniversityManager.Infrastructure.Commands;
+using UniversityManager.Infrastructure.Commands.Users;
+using UniversityManager.Infrastructure.Services;
+
+namespace UniversityManager.Api.Controllers
+{
+    [Authorize]
+    public class UsersController : BaseController
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService,
+            ICommandDispatcher commandDispatcher) : base(commandDispatcher)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var users = await _userService.Browse();
+
+            return Json(users);
+        }
+
+        [HttpGet("{email}")]
+        public async Task<IActionResult> Get(string email)
+        {
+            var user = await _userService.Get(email);
+
+            if (user == null)
+                return NotFound();
+
+            return Json(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]CreateUser command)
+        {
+            await Dispatch(command);
+
+            return Created($"users/{command.Email}", null);
+        }
+    }
+}
---
+++ UniversityManager.Infrastructure/Handlers/Users/CreateUserHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using UniversityManager.Infrastructure.Commands;
+using UniversityManager.Infrastructure.Commands.Users;
+using UniversityManager.Infrastructure.Services;
+
+namespace UniversityManager.Infrastructure.Handlers.Users
+{
+    public class CreateUserHandler : ICommandHandler<CreateUser>
+    {
+        private readonly IUserService _userService;
+
+        public CreateUserHandler(IUserService userService)
+        {
+            _userService = userService;
+        }
+        public async Task Handle(CreateUser command)
+        {
+            await _userService.Register(Guid.NewGuid(), command.Email, command.Username,
+                command.Password, command.Role);
+        }
+    }
+}
